63e26557005eac79748060d6d54674ac604bcc4b
Winnie <2801494637@qq.com>
2023-05-31 16:20:26 +0800

    refactor: remove unnecessary commented codes and update findone method
    Removed unnecessary comments from the comment section of the project.
    Updated the code by replacing findOne({ where: { id } }) with
    findOneBy({ id }) for improved readability and consistency.

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,67 +1,3 @@
-// import { Injectable } from '@nestjs/common';
-// import { InjectRepository } from '@nestjs/typeorm';
-// import { Repository } from 'typeorm';
-// import { Comment } from './entities/comment.entity';
-// import { CreateCommentDto } from './dto/create-comment-dto';
-
-// @Injectable()
-// export class CommentService {
-//   constructor(
-//     @InjectRepository(Comment)
-//     private commentRepository: Repository<Comment>,
-//   ) {}
-
-//   async create(createCommentDto: CreateCommentDto): Promise<Comment> {
-//     const comment = this.commentRepository.create(createCommentDto);
-//     return this.commentRepository.save(comment);
-//   }
-
-//   async findAll(): Promise<Comment[]> {
-//     return this.commentRepository.find();
-//   }
-
-//   async findById(id: number): Promise<Comment> {
-//     return this.commentRepository.findOne(id);
-//   }
-
-//   async update(
-//     id: number,
-//     updateCommentDto: CreateCommentDto,
-//   ): Promise<Comment> {
-//     const comment = await this.commentRepository.preload({
-//       id: +id,
-//       ...updateCommentDto,
-//     });
-//     if (!comment) {
-//       throw new Error('Comment not found');
-//     }
-//     return this.commentRepository.save(comment);
-//   }
-
-//   async delete(id: number): Promise<void> {
-//     const comment = await this.commentRepository.findOne(id);
-//     if (!comment) {
-//       throw new Error('Comment not found');
-//     }
-//     this.commentRepository.remove(comment);
-//   }
-
-//   async findByReviewId(reviewId: number): Promise<Comment[]> {
-//     return this.commentRepository.find({ where: { reviewId } });
-//   }
-
-//   async createForReview(
-//     reviewId: number,
-//     createCommentDto: CreateCommentDto,
-//   ): Promise<Comment> {
-//     const comment = this.commentRepository.create({
-//       ...createCommentDto,
-//       reviewId,
-//     });
-//     return this.commentRepository.save(comment);
-//   }
-// }
-
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -85,7 +21,7 @@ export class CommentService {
   }
 
   async findById(id: number): Promise<Comment> {
-    return this.commentRepository.findOne({ where: { id } });
+    return this.commentRepository.findOneBy({ id });
   }
 
   async update(
@@ -103,7 +39,7 @@ export class CommentService {
   }
 
   async delete(id: number): Promise<void> {
-    const comment = await this.commentRepository.findOne({ where: { id } });
+    const comment = await this.commentRepository.findOneBy({ id });
     if (!comment) {
       throw new Error('Comment not found');
     }
