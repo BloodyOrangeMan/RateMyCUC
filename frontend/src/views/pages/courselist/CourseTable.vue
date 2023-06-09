c3ee620f87a807238c49e84809075593ae9db232
cais-ou <670323039@qq.com>
2023-06-09 23:15:16 +0800

    feat: 🎸 add search for courselist && fix table bugs

--- frontend/src/views/pages/courselist/CourseTable.vue
+++ frontend/src/views/pages/courselist/CourseTable.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { VDataTable } from 'vuetify/labs/VDataTable'
 import { useCourseStore } from '@/stores/courseStore'
+// Define component props
 const props = defineProps({
   departmentName: {
     type: String,
@@ -8,25 +9,67 @@ const props = defineProps({
   },
 })
 
+import { useRouter } from 'vue-router'
+const router = useRouter()
+// Redirect to review page
+const redirectToReview = (classID:number) => {
+  router.push(`/review/${classID}`)
+}
+
+
+// Define table headers
 const headers = [
-  { title: '课程名', key: 'coursename' },
-  { title: '任课教师', key: 'teacher' },
-  { title: '评分', key: 'rate' },
-  { title: '评论数', key: 'numberofrating' },
+  { title: 'Course Name', key: 'coursename' },
+  { title: 'Teacher', key: 'teacher' },
+  { title: 'Rating', key: 'rate' },
+  { title: 'Number of Ratings', key: 'numberofrating' },
+  { title: 'Action', key: 'classID', sortable: false },
 ]
 
+// Search text
+let searchText = ''
+
+// Loading state
+const loading = ref(true)
+
+// Use the course store
 const courseStore = useCourseStore()
+
+// Card details visibility
 const isCardDetailsVisible = ref(false)
 
+// Fetch course list by department name
 const fetchCourseList = async () => {
   await courseStore.fetchCourseListByDepartment(props.departmentName)
 }
 
+// Toggle card details visibility
 const toggleCardDetails = () => {
   isCardDetailsVisible.value = !isCardDetailsVisible.value
   if (isCardDetailsVisible.value) {
-    fetchCourseList()
+    loading.value = true
+    fetchCourseList().then(() => {
+      loading.value = false
+    })
   }
+}
+
+// Filtered course items
+const filteredItems = computed(() => {
+  if (searchText) {
+    const searchTerm = searchText.toLowerCase()
+    return courseStore.courseMap[props.departmentName].filter(
+      item => item.coursename.toLowerCase().includes(searchTerm) || item.teacher.toLowerCase().includes(searchTerm),
+    )
+  } else {
+    return courseStore.courseMap[props.departmentName]
+  }
+})
+
+// Handle search
+const handleSearch = () => {
+  loading.value = true
+  loading.value = false
 }
 </script>
 
@@ -40,10 +83,12 @@ const toggleCardDetails = () => {
           </VCardTitle>
         </VCardItem>
         <VCardActions>
+          <!-- Toggle card details button -->
           <VBtn @click="toggleCardDetails">Details</VBtn>
 
           <VSpacer />
 
+          <!-- Card details visibility toggle button -->
           <VBtn
             icon
             size="small"
@@ -53,26 +98,42 @@ const toggleCardDetails = () => {
           </VBtn>
         </VCardActions>
 
+        <!-- Card details content -->
         <VExpandTransition>
           <div v-show="isCardDetailsVisible">
             <VDivider />
             <VTable>
-              <thead>
-                <tr>
-                  <th class="text-uppercase text-left">课程列表</th>
-                </tr>
-              </thead>
-
               <tbody>
                 <tr>
+                  <!-- Data table -->
                   <td>
+                    <v-text-field
+                      v-model="searchText"
+                      variant="solo"
+                      label="输入搜索 课程/老师名"
+                      prepend-inner-icon="mdi-magnify"
+                      single-line
+                      hide-details
+                      @input="handleSearch"
+                    ></v-text-field>
                     <v-data-table
+                      :search="searchText"
                       :headers="headers"
-                      :items="courseStore.courseMap[departmentName]"
+                      :items="filteredItems"
                       class="elevation-1"
+                      :loading="loading"
                       id="sort-header"
                     >
-                  </v-data-table>
+                      <!-- Custom button content for Action column -->
+                      <template v-slot:item.classID="{ item }">
+                        <v-icon
+                          size="small"
+                          class="me-2"
+                          icon="mdi-pencil"
+                          @click="redirectToReview(item.value.classID)"
+                        ></v-icon>
+                      </template>
+                    </v-data-table>
                   </td>
                 </tr>
               </tbody>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -49,6 +49,8 @@ export default defineConfig({
   },
   server: {
     port: 3001,
+    host: '0.0.0.0',
+    open: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
